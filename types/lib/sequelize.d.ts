a776c233d6d70838ffdf50c2f6c69691eaacd6ea
Jonathan Prévost <contact@jonathanprevost.com>
2021-10-23 11:33:37 +0200

    connect throught node

--- types/lib/sequelize.d.ts
+++ types/lib/sequelize.d.ts
@@ -167,7 +167,7 @@ export interface Config {
   };
 }
 
-export type Dialect = 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'node';
+export type Dialect = 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql';
 
 export interface RetryOptions {
   match?: (RegExp | string | Function)[];
